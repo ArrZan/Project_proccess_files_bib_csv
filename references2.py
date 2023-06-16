7206d540604a815ee6e7e1cf6b3e09401d6abdd8
Josué Zambrano Chávez <joso-eloso@hotmail.com>
2023-06-16 02:07:39 -0500

    Creación de un nuevo archivo references2. Uso de regex para sacar autores y el año de una línea de las referencias.

--- references2.py
+++ references2.py
@@ -1,13 +1,12 @@
 import re
 
-ar = open("scopusRef.csv", encoding="utf8")
+ar = open("scopusRef.csv", encoding="utf8")  # Varible que toma
+# tsodo el archivo
 nomarRef = "archivos\\referenciasContar"
 nomarRefSql = "archivos\\referenciasContar"
 
 bandera = 0
-# arRef=open("referenciasContar.csv","w", encoding="utf8")
-# arRef.write("author,archivo\n")
-# arRef1=open("referenciasContar.sql","w", encoding="utf8")
+
 linea = ar.readline()  # cabecera
 linea = ar.readline()  # primera referencia
 conArc = 1
@@ -15,7 +14,25 @@ authors = []
 conarchivo = 1
 confila = 1
 for linea in ar:
-    print(conArc, linea)
+    # print(conArc, linea)
     refArt = linea.split(";")
     for ref in refArt:
-        pass
+        data = {}
+        tempRef = ref
+        autores1 = re.findall(r'([a-zA-ZÀ-ÿ .-]+,\s([A-Z].-[A-Z].|[A-Z].)+)+,\s', ref) #  83
+        añoArc = re.search(r'([(\d)]{6}){1}', ref) #  85
+
+        # tempRef = tempRef.replace(autores1.group(1),'')
+        # tempRef = tempRef.replace(añoArc.group(1),'')
+        data['autores'] = []
+        if len(autores1) > 1:
+            for value in autores1:
+                data['autores'].append(value[0])
+        else:
+            data['autores'].append(autores1[0][0])
+
+        data['año'] = añoArc.group(1)
+
+        if tempRef[0] == "(":
+            pass
+        ar.close()
