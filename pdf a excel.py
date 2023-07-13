1bdfcfc445409954780a5d681ca3703dbd20c745
Josué Zambrano Chávez <joso-eloso@hotmail.com>
2023-07-13 10:34:57 -0500

    Revert "pdf a excel"
    This reverts commit 791d21231919874463012c180fd2c9f5e91d3fad.

--- pdf a excel.py
+++ pdf a excel.py
@@ -1,25 +1,35 @@
 import csv
 
-with open("archivos/capital.csv") as csv_file:
-    listFile = csv.reader(csv_file,delimiter=';')
+with open("Acccervecerianac.csv") as csv_file:
+    listFile = csv.DictReader(csv_file, delimiter=';')
     bigFile = []
     # Itero el objeto reader para añadirlo en una lista
+    lista2 = []
+    lista = []
     for row in listFile:
-        if row[0] != 'CAPITAL':
-            numero=row[0][0:-4].replace('$','').replace(',','.')
-            bigFile.append(numero+',00')
-    # print(bigFile)
+        bigFile.append(row)
+    for pos, nom in enumerate(bigFile):
+        for clave in nom.keys():
+            if clave == 'CAPITAL':
+                lista.append(nom[clave])
+    for x in lista:
+        numero_sin_ceros = x[:-2]
+        lista2.append(numero_sin_ceros)
+    CONT = 0
+    listanueva = []
+    for cap in bigFile:
+        for x in cap:
+            if x == 'CAPITAL':
+                dicc = {x: lista2[CONT]}
+                cap.update(dicc)
+                CONT += 1
+    print(bigFile)
 
-#
-with open("archivos/nuevoprueba.csv", "w") as new_csv_file:
-    for i in bigFile:
-        new_csv_file.write(i +'\n')
+with open("nuevoprueba.csv", "w", newline='') as new_csv_file:
+    fieldnames = bigFile[0].keys()
+    writer = csv.DictWriter(new_csv_file, fieldnames=fieldnames, delimiter=';')
 
+    writer.writeheader()  # Escribir la fila de encabezados
+    writer.writerows(bigFile)  # Escribir los datos modificados
 
-#     fieldnames = bigFile[0].keys()
-#     writer = csv.DictWriter(new_csv_file, fieldnames=fieldnames, delimiter=';')
-#
-#     writer.writeheader()  # Escribir la fila de encabezados
-#     writer.writerows(bigFile)  # Escribir los datos modificados
-#
-# print("El archivo CSV ha sido guardado con los cambios.")
+print("El archivo CSV ha sido guardado con los cambios.")
